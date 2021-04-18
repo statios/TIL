012cdb99d4667d3c7b2d52e7d1f55121b83eef52
Stat <statios@kakao.com>
2021-04-18 23:46:03 +0900

    feat: create swipe back detection

---
+++ Example_SwipeBackDetection/Example_SwipeBackDetection/NavigationViewController.swift
@@ -0,0 +1,8 @@
+//
+//  NavigationViewController.swift
+//  Example_SwipeBackDetection
+//
+//  Created by Stat on 2021/04/16.
+//
+
+import Foundation
---
+++ Example_SwipeBackDetection/Example_SwipeBackDetection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Example_SwipeBackDetection
+//
+//  Created by Stat on 2021/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
